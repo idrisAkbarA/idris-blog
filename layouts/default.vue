61734b85bed9510333f327453e3f393a06463e3d
Idris Akbar Adyusman <idrissmp32@gmail.com>
2021-08-26 22:45:43 +0700

    landing page

---
+++ layouts/default.vue
@@ -0,0 +1,64 @@
+<template>
+  <vue-scroll :ops="scrollOps">
+    <v-app dark>
+      <v-main class="landing-bg">
+        <nuxt />
+      </v-main>
+    </v-app>
+  </vue-scroll>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      scrollOps: {
+        scrollPanel: {
+          easing: 'easeInQuad',
+          speed: 800,
+          scrollingX: false,
+        },
+        bar: {
+          background: '#FFEBEE',
+        },
+        vuescroll: {
+          mode: 'native',
+          wheelScrollDuration: 0,
+          locking: true,
+        },
+      },
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
+<style>
+.landing-bg {
+  /* background: red !important; */
+  background: orangered;
+  background-image: url('/landing-bg.svg') !important;
+  background-size: cover !important;
+}
+.bg-pattern {
+  background: url('/pattern.svg') repeat;
+  background-size: 400px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <v-container
+    max-width="200px"
+    class="fill-height"
+    :style="windowWidth<=600?'width:100%':'width:60%'"
+  >
+    <!-- <v-row>
+      <h1>Hello</h1>
+    </v-row> -->
+    <v-row>
+      <v-col
+        cols="12"
+        md="4"
+        lg="4"
+        xl="4"
+      >
+        <v-hover v-slot="{ hover }">
+
+          <v-card
+            to="/portofolio-1"
+            flat
+            max-width="200px"
+            min-height="300px"
+            tile
+            :class="hover?'dropshadowclass deep-orange--text text--lighten-5':'nodropshadowclass'"
+            class="mx-auto d-flex flex-column justify-end pa-4 "
+          >
+            <div>
+              <h1>Portofolio</h1>
+            </div>
+          </v-card>
+        </v-hover>
+      </v-col>
+      <v-col
+        cols="12"
+        md="4"
+        lg="4"
+        xl="4"
+      >
+
+        <v-hover v-slot="{ hover }">
+
+          <v-card
+            to="/about"
+            flat
+            max-width="200px"
+            min-height="300px"
+            tile
+            :class="hover?'dropshadowclass deep-orange--text text--lighten-5':'nodropshadowclass'"
+            class="mx-auto d-flex flex-column justify-end pa-4 "
+          >
+            <div>
+              <span>About me:</span>
+              <h1>Idris Akbar Adyusman</h1>
+            </div>
+          </v-card>
+        </v-hover>
+
+      </v-col>
+      <v-col
+        cols="12"
+        md="4"
+        lg="4"
+        xl="4"
+      >
+
+        <v-hover v-slot="{ hover }">
+
+          <v-card
+            to="/blog"
+            flat
+            max-width="200px"
+            min-height="300px"
+            tile
+            :class="hover?'dropshadowclass deep-orange--text text--lighten-5':'nodropshadowclass'"
+            class="mx-auto d-flex flex-column justify-end pa-4 "
+          >
+            <div>
+              <h1>Blog</h1>
+            </div>
+          </v-card>
+        </v-hover>
+
+      </v-col>
+    </v-row>
+
+  </v-container>
+</template>
+
+<style lang="sass">
+$hoverColor: #FF9800
+.card-menu-heading
+  font-family: 'Playfair Display', serif
+.dropshadowclass
+
+  border: 0px white !important
+  transition: 0.5s ease
+  -moz-box-shadow: 0px
+  -webkit-box-shadow: 0px
+  box-shadow: 0px
+
+.nodropshadowclass
+  border: solid 4px white !important
+  background: rgba(255, 255, 255, 0.15) !important
+  transition: 0.5s ease
+  -moz-box-shadow: 15px 15px 0px $hoverColor !important
+  -webkit-box-shadow: 15px 15px 0px $hoverColor !important
+  box-shadow: 15px 15px 0px $hoverColor !important
+</style>>
+
+
